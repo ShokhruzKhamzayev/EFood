978aa44c4319a6ab50a0d453ad76b809fde6e738
Shoxruz <forpk432@gmail.com>
2023-11-04 12:17:00 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Delivery from "./pages/Delivery";
+import './css/style.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <Home/>
+        <Category/>
+        <Delivery/>
+      </div>
     </div>
   );
 }
